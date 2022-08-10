10b0de166c93282ef7ec4a9e30ce7067842ab521
debbie <deborahizidor@gmail.com>
2022-08-11 00:17:27 +0100

    my tour guide

---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div className='title'>
+      <h2>...loading</h2>
+    </div>
+  );
+};
+
+export default loading;
---
+++ src/Tour.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+const Tour = ({ id, image, name, price, info, removeTour }) => {
+  const [readMore, setReadmore] = useState(false);
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name}></img>
+      <footer>
+        <div className='tour-info'>
+          <h4>{name} </h4>
+          <h4 className='tour-price'>{price} </h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadmore(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
+
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          Not Intrested
+        </button>
+      </footer>
+    </article>
+  );
+};
+export default Tour;
